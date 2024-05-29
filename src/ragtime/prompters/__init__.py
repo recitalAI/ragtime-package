e7ed650b5f0c803a3d3f0d5f40b9e9711d2e585c
ImadZaoug <114984003+ImadZaoug@users.noreply.github.com>
2024-05-29 14:05:10 +0100

    Add files via upload
    Adding prompter for the QuestionGenerator classe

--- src/ragtime/prompters/__init__.py
+++ src/ragtime/prompters/__init__.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
-from ragtime.base.prompter import Prompter
+from ragtime.base.prompter import *
 
 from ragtime.prompters.answer.base import PptrAnsBase
 from ragtime.prompters.answer.with_retriever import PptrAnsWithRetrieverFR
 from ragtime.prompters.fact.base_fr import PptrFactsFR
 from ragtime.prompters.eval.base_fr import PptrEvalFR
+from ragtime.prompters.question.base_fr import PptrQuestionsFR
 
 table: dict = {
+    "PptrQuestionsFR": (lambda: PptrQuestionsFR()),
     "PptrAnsBase": (lambda: PptrAnsBase()),
     "PptrFactsFR": (lambda: PptrFactsFR()),
     "PptrEvalFR": (lambda: PptrEvalFR()),
