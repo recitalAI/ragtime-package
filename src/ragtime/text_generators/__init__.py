9d4cb087a76bf767db266bc1d40c1e864c8e9c0f
ImadZaoug <114984003+ImadZaoug@users.noreply.github.com>
2024-05-29 14:07:12 +0100

    Creating the QuestionGenerator class

--- src/ragtime/text_generators/__init__.py
+++ src/ragtime/text_generators/__init__.py
@@ -1,5 +1,6 @@
-from ragtime.base.text_generator import TextGenerator
+from ragtime.base.text_generator import *
 
 from ragtime.text_generators.answer_generator import AnsGenerator
 from ragtime.text_generators.eval_generator import EvalGenerator
 from ragtime.text_generators.fact_generator import FactGenerator
+from ragtime.text_generators.question_generator import QuestionGenerator
